1db574d6c1574ee32574077790a14c7d39842c97
Necroneco <caibinqing@live.com>
2023-01-07 00:38:10 +0800

    Add support for Aqara Smart Door Lock D200

--- custom_components/aqara_gateway/core/lock_data.py
+++ custom_components/aqara_gateway/core/lock_data.py
@@ -12,6 +12,9 @@ DEVICE_MAPPINGS = {
     "lumi.lock.acn03": SUPPORT_ALARM | SUPPORT_DOORBELL,
     "aqara.lock.eicn01": SUPPORT_ALARM | SUPPORT_DOORBELL,
     "aqara.lock.acn001": SUPPORT_ALARM | SUPPORT_DOORBELL,
+    "aqara.lock.acn004": (
+        WITH_LI_BATTERY | SUPPORT_ALARM |
+        SUPPORT_DOORBELL | SUPPORT_WIFI),
     "aqara.lock.wbzac1": (
         WITH_LI_BATTERY | SUPPORT_ALARM |
         SUPPORT_DOORBELL | SUPPORT_WIFI | SUPPORT_CAMERA),
