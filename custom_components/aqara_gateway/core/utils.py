bdfef5405b26aafc9432b85fcbea52de1550653d
Ashot Tonoyan <ashot.tonoyan@gmail.com>
2023-01-16 23:56:33 +0400

    Fixed brightness and color temperature for L1-350

--- custom_components/aqara_gateway/core/utils.py
+++ custom_components/aqara_gateway/core/utils.py
@@ -275,7 +275,6 @@ DEVICES = [{
     'ikea.light.led1546g12': ["IKEA", "Bulb E27 950 lm", "LED1546G12"],
     'ikea.light.led1536g5': ["IKEA", "Bulb E14 400 lm", "LED1536G5"],
     'ikea.light.led1537r6': ["IKEA", "Bulb GU10 400 lm", "LED1537R6"],
-    'lumi.light.acn003': ["Aqara", "L1-350 Ceiling Light", "ZNXDD01LM"],
     'params': [
         ['4.1.85', 'power_status', 'light', 'light'],
         ['14.1.85', 'light_level', 'brightness', None],
@@ -286,6 +285,7 @@ DEVICES = [{
     'lumi.light.cwac02': ["Aqara", "Bulb T1", "ZNLDP13LM"],  # @Kris
     'lumi.light.acn014': ["Aqara", "Bulb T1", "ZNLDP14LM"],
     'lumi.light.acn015': ["Aqara", "Sun Light H1", "QKD01LM"],
+    'lumi.light.acn003': ["Aqara", "L1-350 Ceiling Light", "ZNXDD01LM"],
     'params': [
         ['4.1.85', 'power_status', 'light', 'light'],
         ['1.6.85', None, 'ms_to_turn_on', None],
