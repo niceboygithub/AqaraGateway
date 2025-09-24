cb3261777370490c47465b42f0a2f54fe75cc716
Maksim Kovalyov <kaizer@kai-zer.ru>
2025-09-24 19:57:28 +0800

    light

--- custom_components/aqara_gateway/light.py
+++ custom_components/aqara_gateway/light.py
@@ -175,8 +175,10 @@ class GatewayLight(GatewayGenericDevice, LightEntity):
             self._attr_hs_color = kwargs[ATTR_HS_COLOR]
 
         if (ATTR_HS_COLOR in kwargs or ATTR_BRIGHTNESS in kwargs):
+            _LOGGER.info('payload = ', payload)
             if self._attr_hs_color:
-                payload[ATTR_HS_COLOR] = color_util.color_temperature_kelvin_to_mired(self._attr_color_temp_kelvin)
+                _LOGGER.info('self._attr_color_temp_kelvin = ', self._attr_color_temp_kelvin)
+                # payload[ATTR_HS_COLOR] = color_util.color_temperature_kelvin_to_mired(self._attr_color_temp_kelvin)
                 rgb = color_util.color_hs_to_RGB(*self._attr_hs_color)
                 rgba = (self._attr_brightness,) + rgb
                 if isinstance(self._attr_brightness, int):
